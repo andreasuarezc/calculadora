da4e7112714e910040260c43cd78cc209e3c65e6
Andrea <andrea0795@gmail.com>
2021-02-18 19:51:47 -0500

    Se definieron multiples casos de entrada para las pruebas unitarias

--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -2,6 +2,8 @@ package co.com.sofka.app.calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -66,5 +68,54 @@ class BasicCalculatorTest {
 
         // Assert
         assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    public void severalSums(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.sum(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+    @DisplayName("Testing several restas")
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    @CsvSource({
+            "4,    1,   3",
+            "2,    1,   1",
+            "49,  2, 47",
+            "10,  3, 7"
+    })
+    public void severalRestas(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.restar(first, second),
+                () -> first + " - " + second + " should equal " + expectedResult);
+    }
+    @DisplayName("Testing several multiplicaciones")
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    @CsvSource({
+            "0,    1,   0",
+            "1,    2,   2",
+            "49,  1, 49",
+            "1,  100, 100"
+    })
+    public void severalMultiplicaciones(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.multiplicar(first, second),
+                () -> first + " * " + second + " should equal " + expectedResult);
+    }
+    @DisplayName("Testing several divisiones")
+    @ParameterizedTest(name = "{0} / {1} = {2}")
+    @CsvSource({
+            "3,    1,   3",
+            "6,    2,   3",
+            "49,   0,   0",
+            "1,    1,   1"
+    })
+    public void severalDivisiones(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.dividir(first, second),
+                () -> first + " / " + second + " should equal " + expectedResult);
     }
 }
